d05f5760c1f40069c926e7a7a07f773742706962
LinkOFF <linkoff1994@gmail.com>
2024-10-31 18:59:21 +0300

    SotD Hella Remastered data.vfs Extraction Script

--- sotd_hella_remastered_vfs3_extract.py
+++ sotd_hella_remastered_vfs3_extract.py
@@ -1,9 +1,14 @@
 import os, struct
 
+class DirEntry:
+    def __init__(self, f):
+        self.index, self.var04, self.var08, self.var0C = struct.unpack('<4i',  f.read(0x10))
+        self.var10, self.var14, self.var18 = struct.unpack('<iiI',  f.read(0xC))
+
 class FileEntry:
     def __init__(self, f):
         self.offset, self.compressedSize, self.decompressedSize = struct.unpack('<3Q',  f.read(0x18))
-        self.unk18, self.filenameIndex, self.unk20, self.unk24, self.unk26 = struct.unpack('<3I2h',  f.read(0x10))
+        self.unk18, self.filenameIndex, self.dirIndex, self.unk24, self.unk26 = struct.unpack('<3I2h',  f.read(0x10))
         
 def readcstr(f):
     cstr = bytearray()
@@ -13,36 +18,51 @@ def readcstr(f):
             return str(cstr, "utf-16")
         cstr += ch
         
+def align(var):
+    if var % 16 != 0:
+        return var + (16 - var % 16)
+    return var
+        
 def read_filenames(f, offset):
     cur = f.tell()
     f.seek(offset)
-    count = struct.unpack('<I', f.read(4))[0]
-    filenames = []
-    for i in range(count):
-        filenames.append(readcstr(f))
+    file_count = struct.unpack('<I', f.read(4))[0]
+    dirs = []
+    files = []
+    for i in range(file_count):
+        files.append(readcstr(f))
+    dir_count = struct.unpack('<I', f.read(4))[0]
+    for i in range(dir_count):
+        dirs.append(readcstr(f))
     f.seek(cur)
-    return filenames
+    return (files, dirs)
 
 def extract(vfs_file):
     f = open(vfs_file, 'rb')
-    f.seek(0x7c)
-    count = struct.unpack('<I', f.read(4))[0]
+    magic, start_offset, dir_count, pad = struct.unpack('<4I', f.read(16))
+    if magic != 0x33534656:
+        exit()
+    dirs = []
+    for i in range(dir_count):
+        dirs.append(DirEntry(f))
+    count = dirs[len(dirs)-1].var18
     data_start = f.tell() + (count * 0x28) + (0x8 * 3)
+    data_start = align(data_start)
+    print(data_start)
     entries = []
     for i in range(count):
         entries.append(FileEntry(f))
     f.read(16)
     filename_offset = struct.unpack('<Q', f.read(8))[0]
-    filenames = read_filenames(f, filename_offset)
-    out_dir = os.path.splitext(vfs_file)[0]
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    filenames, dirnames = read_filenames(f, filename_offset)
     for entry in entries:
-        filename = filenames[entry.filenameIndex]
-        print('Processing: %s' % filename)
+        if not os.path.exists(dirnames[entry.dirIndex]):
+            os.makedirs(dirnames[entry.dirIndex])
+        filepath = '%s/%s' % (dirnames[entry.dirIndex], filenames[entry.filenameIndex])
+        print('Processing: %s' % (filepath))
         f.seek(data_start + entry.offset)
         data = f.read(entry.decompressedSize)
-        r = open('%s/%s' % (out_dir, filenames[entry.filenameIndex]), 'wb')
+        r = open(filepath, 'wb')
         r.write(data)
         r.close()
     f.close()
